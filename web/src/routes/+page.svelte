318844db3001b62710c2b10a690f04900863a03e
Pascal d'Achard van Enschut <pasdachard@gmail.com>
2023-07-06 18:37:41 +0200

    Square chess board ish

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,1 +1,7 @@
-Hiiii
+
+
+<div class='w-full sm:w-3/4 max-w-3xl mx-auto aspect-square'>
+	<div class='h-full bg-red-800 '>
+		hi
+	</div>
+</div>
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -4,20 +4,6 @@ const config = {
 
 	theme: {
 		extend: {
-			colors: {
-				'naia-teal': '#349D84',
-				'naia-teal-faded': '#3F7A74',
-				'naia-pink-l': '#FFC1C1',
-				'naia-pink-m': '#FC9494',
-				'naia-pink-d': '#5A2323',
-				'naia-orange': '#FFD7B4',
-				'naia-gray-l': '#F1F1F1',
-				'naia-gray-m': '#E2E2E2',
-				'naia-gray-d': '#6B6969',
-				'naia-blue': "#379FFF",
-				'naia-red': "#E10000",
-				'naia-shadow': "rgba(0,0,0,0.51)",
-			}
 
 		}
 	},
